edd5c6a62046c173bd84cd6b88dc478561544e18
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-05-15 11:17:21 +0530

    add settings/contact info  page

---
+++ tests/unit/routes/settings/contact-info-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:settings/contact-info', 'Unit | Route | settings/contact info', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
