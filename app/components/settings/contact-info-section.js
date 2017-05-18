5416cbc1eeed38e376795fe28b52ed048610d560
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-05-18 19:00:39 +0530

    something broken

--- app/components/settings/contact-info-section.js
+++ app/components/settings/contact-info-section.js
@@ -1,6 +1,83 @@
 import Ember from 'ember';
+import FormMixin from 'open-event-frontend/mixins/form';
 
 const { Component } = Ember;
 
-export default Component.extend({
+export default Component.extend(FormMixin, {
+
+  identification : '',
+  phone       : '',
+  isLoading      : false,
+
+  getValidationRules() {
+    return {
+      inline : true,
+      delay  : false,
+      on     : 'blur',
+      fields : {
+        identification: {
+          identifier : 'email',
+          rules      : [
+            {
+              type   : 'empty',
+              prompt : this.i18n.t('Please enter your email ID')
+            },
+            {
+              type   : 'email',
+              prompt : this.i18n.t('Please enter a valid email ID')
+            }
+          ]
+        },
+        phone: {
+          identifier : 'phone',
+          rules      : [
+            {
+              type   : 'empty',
+              prompt : this.i18n.t('Please enter your phone')
+            }
+          ]
+        }
+      }
+    };
+  },
+
+  actions: {
+    submit() {
+      this.onValid(() => {
+        let credentials = this.getProperties('identification', 'phone'),
+            authenticator = 'authenticator:jwt';
+            console.log(credentials);
+
+        this.set('errorMessage', null);
+        this.set('isLoading', true);
+
+        this.get('session')
+          .authenticate(authenticator, credentials)
+          .then(() => {
+            this.get('loader').get('/users/me').then(data => {
+              this.get('session').set('data.currentUser', data);
+            });
+          })
+          .catch(reason => {
+            if (reason.hasOwnProperty('code') && reason.code === 401) {
+              this.set('errorMessage', this.i18n.t('Your credentials were incorrect.'));
+            } else {
+              this.set('errorMessage', this.i18n.t('An unexpected error occurred.'));
+            }
+            this.set('isLoading', false);
+          })
+          .finally(() => {
+            this.set('phone', '');
+          });
+      });
+    }
+  },
+
+  didInsertElement() {
+    if (this.get('session.newUser')) {
+      this.set('newUser', this.get('session.newUser'));
+      this.set('identification', this.get('session.newUser'));
+      this.set('session.newUser', null);
+    }
+  }
 });
