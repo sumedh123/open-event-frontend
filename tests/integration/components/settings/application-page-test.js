7c2b62681404918d510a94cc480ce28392e2ec7d
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-05-18 11:12:57 +0530

    follow naming conventions and modify test files

--- tests/integration/components/settings/application-page-test.js
+++ tests/integration/components/settings/application-page-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('settings/application-page', 'Integration | Component | settings/application page', {
+moduleForComponent('settings/application-section', 'Integration | Component | settings/application page', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{settings/application-page}}`);
+  this.render(hbs`{{settings/application-section}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#settings/application-page}}
+    {{#settings/application-section}}
       template block text
-    {{/settings/application-page}}
+    {{/settings/application-section}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
--- tests/integration/components/settings/email-preferences-test.js
+++ tests/integration/components/settings/email-preferences-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('settings/email-preferences', 'Integration | Component | settings/email preferences', {
+moduleForComponent('settings/email-preferences-section', 'Integration | Component | settings/email preferences', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{settings/email-preferences}}`);
+  this.render(hbs`{{settings/email-preferences-section}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#settings/email-preferences}}
+    {{#settings/email-preferences-section}}
       template block text
-    {{/settings/email-preferences}}
+    {{/settings/email-preferences-section}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
