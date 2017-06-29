d80fba93cfce34abeef76ab139b44f7684ef104e
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-06-29 17:07:55 +0530

    testing

--- tests/helpers/component-helper.js
+++ tests/helpers/component-helper.js
@@ -2,11 +2,9 @@ import { moduleForComponent, test } from 'ember-qunit';
 import destroyApp from './destroy-app';
 import startApp from './start-app';
 import Ember from 'ember';
-import { routing } from 'ember-routing';
 import l10nTestHelper from 'ember-l10n/test-helpers';
 import fragmentTransformInitializer from 'open-event-frontend/initializers/model-fragments';
 import L10n from 'ember-l10n/services/l10n';
-import RoutingService from 'ember-routing/services/routing';
 const { run, getOwner } = Ember;
 
 export default function(path, name, testCase = null) {
@@ -16,8 +14,6 @@ export default function(path, name, testCase = null) {
     beforeEach() {
       this.register('service:l10n', L10n);
       this.inject.service('l10n', { as: 'l10n' });
-      this.register('service:-routing', routing);
-      this.inject.service('-routing', { as: 'routing' });
       this.application = startApp();
       l10nTestHelper(this);
       run(() => fragmentTransformInitializer.initialize(getOwner(this)));
